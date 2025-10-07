cd2f34b303815660a77b388ddb13e39ed6fe7ab0
An Hoang Nguyen <nguyenan.sadek@outlook.com>
2025-10-07 21:29:53 +0700

    Nghi ra duoc template

---
+++ _8_queens.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+
+bool check(){
+	
+}
+
+
+int main(){
+	/*
+	De tranh truong hop lon xon giua thu tu va chi so
+	them 1 hang, 1 cot cho ca ban co lan ket qua.
+	Y tuong: Moi phan tu trong ban co co gia tri [-8; 1]
+		Trong do:
+			1: Hien co quan hau
+			0: Co the dat quan hau
+			<0: Bi cam dat quan hau
+		Muc dich
+			Trong truong hop muon cap nhat thi co the cap nhat de dang
+			chi tiet trong README.md
+	*/
+	// Khoi tao mang 2 chieu 9 x 9 voi cac phan tu deu bang 0
+	int board[9][9] = {0};
+	
+	// Khoi tao mang ket qua voi cac phan tu deu bang 0
+	// Neu result[i] = 0 thi chua xet hang i
+	int result[9] = {0};
+
+	
+	return 0;
+}
