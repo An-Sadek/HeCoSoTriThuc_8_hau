e945161bfd8ed5789d9740077767d151ffaf3315
An Hoang Nguyen <nguyenan.sadek@outlook.com>
2025-11-04 11:27:43 +0700

    Quen bo cac ham trong header

--- _8_queens.h
+++ _8_queens.h
@@ -26,13 +26,4 @@ public:
     void solve(const int start_col);
 };
 
-bool in_bound(const int value);
-bool check(const int board[8][8], const int row, const int col);
-void update(int board[8][8], const int row, const int col, const int sign);
-bool try_col(int board[8][8], const int row, const int col, int result[8]);
-void solve(int board[8][8], const int start_col, int result[8]);
-void print_board_queen(const int board[8][8]);
-void print_board_state(const int board[8][8]);
-void print_result(const int result[8]);
-
 #endif
